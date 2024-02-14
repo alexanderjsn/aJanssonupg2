323744c9c2ab8c5ab4b187ec44c1f6945d3f8810
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-02-14 15:58:01 +0100

    la till klass struktur, mappar och servlets

---
+++ src/main/java/org/example/controller/attendanceServlet.java
@@ -0,0 +1,10 @@
+package org.example.controller;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/attendance")
+public class attendanceServlet extends HttpServlet {
+}
---
+++ src/main/java/org/example/controller/viewCourseServlet.java
@@ -0,0 +1,8 @@
+package org.example.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/courses")
+public class viewCourseServlet extends HttpServlet {
+}
