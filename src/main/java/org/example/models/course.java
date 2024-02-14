2feaa5edb9aa3575e70b49bc529f021c8bd748ad
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-02-14 16:01:03 +0100

    java beans, skapade för kurs och student

--- src/main/java/org/example/models/course.java
+++ src/main/java/org/example/models/course.java
@@ -1,4 +1,41 @@
 package org.example.models;
 
 public class course {
+    private int courseId;
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYhp() {
+        return yhp;
+    }
+
+    public void setYhp(int yhp) {
+        this.yhp = yhp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String name;
+    private int yhp;
+    private String description;
 }
--- src/main/java/org/example/models/student.java
+++ src/main/java/org/example/models/student.java
@@ -1,4 +1,52 @@
 package org.example.models;
 
+
+// kapslar in motsvarade databas namn
 public class student {
+    private int student_id;
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    private String Fname;
+    private String Lname;
+    private String city;
+    private String hobby;
 }
