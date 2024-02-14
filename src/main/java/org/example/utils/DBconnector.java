13230323e4950bcc0322265e58fea9c40283b901
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-02-14 16:07:44 +0100

    skapade  connection till sql

--- src/main/java/org/example/utils/DBconnector.java
+++ src/main/java/org/example/utils/DBconnector.java
@@ -1,4 +1,28 @@
 package org.example.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBconnector {
+
+    // skapar en connection med null värde
+    private static Connection connection = null;
+
+
+    private DBconnector(){}
+
+    public static Connection getConnection() {
+        try{
+            // kollar om connection finns eller om en är stängds
+            if(connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gritacademy", "root", "");
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }return connection;
+    }
 }
